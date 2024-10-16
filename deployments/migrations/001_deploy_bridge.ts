6071594f4e12717b8a6636d7b06186124a9bd744
Karuizawa-Kei <tuan.hoang@sotatek.com>
2024-10-16 15:28:52 +0700

    fix: add test

--- deployments/migrations/001_deploy_bridge.ts
+++ deployments/migrations/001_deploy_bridge.ts
@@ -15,7 +15,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment): Pr
       owner: deployer,
       execute: {
         methodName: "initialize",
-        args: ["0xa314AEC0369ca4549b3D9f4292d09F670B952F3F", "0", "100000000000000000000", [deployer, "0xa180A8dc9A4966cbaDd333c175c186163d03BE78", "0x1BC543Cabb710E1292BdDdAB4059fFC7a52A1dF3"], 2], // change me when deploy production
+        args: ["0x935DFD95a23Bf87b5B6E5eEF612bD1fDDB0365B6", "0", "100000000000000000000", [deployer, "0xa180A8dc9A4966cbaDd333c175c186163d03BE78", "0x1BC543Cabb710E1292BdDdAB4059fFC7a52A1dF3"], 2], // change me when deploy production
       },
     },
   });
